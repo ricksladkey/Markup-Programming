a3bd053726d9ece74d9e8d3f29edb7d024f47953
Rick Sladkey <rick.sladkey@gmail.com>
2011-03-20 15:31:09 +0000

    - Add markup-visible KeyValuePair for dictionary initialization.
    git-tfs-id: [https://tfs.codeplex.com/tfs/tfs21]$/programmability/Markup.Programming;C7539

---
+++ Markup.Programming/External/Objects/KeyValuePair.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Windows;
+using Markup.Programming.Core;
+
+namespace Markup.Programming
+{
+    public class KeyValuePair : ValueExpression
+    {
+        public object Key
+        {
+            get { return (object)GetValue(KeyProperty); }
+            set { SetValue(KeyProperty, value); }
+        }
+
+        public static readonly DependencyProperty KeyProperty =
+            DependencyProperty.Register("Key", typeof(object), typeof(KeyValuePair), null);
+
+        public string KeyPath { get; set; }
+
+        private PathExpression keyPathExpression = new PathExpression();
+        protected PathExpression KeyPathExpression { get { return keyPathExpression; } }
+
+        public string ValuePath { get { return Path; } set { Path = value; } }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            Attach(KeyProperty);
+        }
+
+        protected override object OnEvaluate(Engine engine)
+        {
+            return new DictionaryEntry(engine.Evaluate(KeyProperty, KeyPath, KeyPathExpression), engine.Evaluate(ValueProperty, ValuePath, PathExpression));
+        }
+    }
+}
