b63e75c6b838cfb79583c7f075324ec4454b4931
Rick Sladkey <rick.sladkey@gmail.com>
2012-11-10 02:05:01 -0500

    Create NuGet package.

---
+++ make-package.cmd
@@ -0,0 +1,6 @@
+mkdir package
+mkdir package\lib
+mkdir package\lib\net4
+mkdir package\lib\sl4
+copy bin\WPF4\Release\Markup.Programming.dll package\lib\net4
+copy bin\Silverlight4\Release\Markup.Programming.dll package\lib\sl4
